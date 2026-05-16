b57729349936a59f40d39713317a5e97500ebdfe
Jep <jeffo.productions@gmail.com>
2026-05-16 14:21:59 +1000

    Initial commit: MVK Winner Selection Tool

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MamaVusGiveaway from './MamaVusGiveaway.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MamaVusGiveaway />
+  </React.StrictMode>
+);
